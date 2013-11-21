b375140e79a6a82e93c19128f18a4ca40872606b
Robbie <robbie@xcellweb.co.uk>
2013-11-21 14:45:57 +0000

    added perl

---
+++ PERL/sms.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -T
+ 
+use strict;
+use LWP::UserAgent;
+use HTTP::Request::Common;
+ 
+# Data for text message
+my $hash = "Enter Your Hash here";
+my $to = "07774545985";
+my $message = "This is a test message"; # Text message content
+my $ua = LWP::UserAgent->new();
+ 
+my $res = $ua->request
+(
+ POST 'http://www.smspi.co.uk/send/',
+ Content_Type  => 'application/x-www-form-urlencoded',
+ Content       => [ 'to'       => $to,
+                    'message'    => $message,
+                    'hash'    => $hash
+                  ]
+);
+ 
+if ($res->is_error) { die "HTTP Error\n"; }
+print "Response:\n\n" . $res->content . "\n\n";
