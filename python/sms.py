dd180b164b2486a6d0cce9206f0ea3b7f3092f2b
Robbie <robbie@xcellweb.co.uk>
2013-11-22 16:28:29 +0000

    not accepting hash possible fix

--- python/sms.py
+++ python/sms.py
@@ -2,10 +2,8 @@
 import urllib
   
 def sendSMS(to, message, hash):
-    params = {'to': to,
-        'message' : message, 'hash': "YOURHASHHERE"}
-    f = urllib.urlopen('http://www.smspi.co.uk/send/?'
-        + urllib.urlencode(params))
+    params = urllib.urlencode({'to': to, 'message' : message, 'hash': hash})
+	f = urllib.urlopen('http://www.smspi.co.uk/send/', params)
     return (f.read(), f.code)
   
 resp, code = sendSMS('07733665594', 'send via www.smspi.co.uk')
