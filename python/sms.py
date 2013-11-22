983ba5d327eb79fab8b7a0e66c9419db3b661cf6
Robbie <txt3rob@gmail.com>
2013-11-22 16:30:47 +0000

    Update sms.py

--- python/sms.py
+++ python/sms.py
@@ -6,5 +6,5 @@ def sendSMS(to, message, hash):
     f = urllib.urlopen('http://www.smspi.co.uk/send/', params)
     return (f.read(), f.code)
   
-resp, code = sendSMS('07733665594', 'send via www.smspi.co.uk')
+resp, code = sendSMS('07733665594', 'send via www.smspi.co.uk', 'HASH')
 print resp
