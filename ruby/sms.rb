d4fd952e7177aa3dc7b06396c5d5936299cc8af4
Robbie <txt3rob@gmail.com>
2015-03-27 14:09:51 +0000

    Update sms.rb

--- ruby/sms.rb
+++ ruby/sms.rb
@@ -6,7 +6,7 @@ messsage = "Your message"
 numbers = "447123456789"
 
  
-requested_url = 'http://www.smspi.co.uk/send/' +
+requested_url = 'http://www.smspi.co.uk/api/' +
 "&hash=" + hash + 
 "&to=" + numbers + "&message=" + URI.escape(message)
              
