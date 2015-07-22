f33736348316d52ef5c8cb938c9aca9f63aef111
Robbie <txt3rob@gmail.com>
2015-07-22 12:00:58 +0100

    Updated
    Updated to show errors and parse the json response from the API.

--- Examples/Python/send_sms.py
+++ Examples/Python/send_sms.py
@@ -4,7 +4,7 @@
 #
 # Author : Robert Wiggins
 # Site   : http://www.smspi.co.uk
-# Date   : 01/10/2013
+# Date   : 22/07/2015
 #
 # Altered Script from Matt Hawkins of http://www.raspberrypi-spy.co.uk/
 # 
@@ -15,15 +15,19 @@
 import urllib      # URL functions
 import urllib2     # URL functions
 import sys		   # System Function
+import json		   # Json Parse
+
+# Validate the arguments have been set
+if(len(sys.argv) < 2):
+	exit('To use this please do the following...... python send_sms.py 07712312313 "this is the message required to be sent"') 
 
 # Arguments Taken from command line
-
 to = sys.argv[1]
 message = sys.argv[2]
-hash = "YOURHASH"
+
 ### Set to Yes if you wish to send a flash (class 0) message
 FLASH = "NO"
-
+hash = "YOUR SMSPI API HASH"
 
 #-----------------------------------
 # No need to edit anything below this line
@@ -47,7 +51,14 @@ try:
   response = urllib2.urlopen(req)
   response_url = response.geturl()
   if response_url==url:
-    print response.read()
+	data = json.load(response)
+	errorvalue = data[0]['error']
+	if errorvalue  == False:
+	 info = data[0]['info']
+	 print "SMS Status: " + info + "\n" + "To: " + to
+	else:
+	 smserror = data[0]['message']
+	 print "Error: " + smserror
 except urllib2.URLError, e:
   print 'Send failed!'
   print e.reason
