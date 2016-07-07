14c60f702425a3cae5846910eae01ca391fb7d61
Robbie <txt3rob@gmail.com>
2016-07-07 09:33:10 +0100

    Update sms.pl

--- Examples/PERL/sms.pl
+++ Examples/PERL/sms.pl
@@ -1,25 +1,20 @@
 #!/usr/bin/perl -T
  
-use strict;
 use LWP::UserAgent;
 use HTTP::Request::Common;
- 
-# Data for text message
-my $hash = "Enter Your Hash here";
-my $to = "07774545985";
-my $message = "This is a test message"; # Text message content
+
+my $url = URI->new("http://www.smspi.co.uk/api/send/");
+$url->query_form({"report"=>"no","to"=>"YOURNUMBER","message"=>"YOURMESSAGE","hash"=>"YOURHASH","flash"=>"no"});
+
 my $ua = LWP::UserAgent->new();
- 
-my $res = $ua->request
-(
- POST 'http://www.smspi.co.uk/api/send/',
- Content_Type  => 'application/x-www-form-urlencoded',
- Content       => [ 'to'       => $to,
-                    'message'    => $message,
-                    'hash'    => $hash,
-                    'flash' => 'no'
-                  ]
-);
- 
-if ($res->is_error) { die "HTTP Error\n"; }
-print "Response:\n\n" . $res->content . "\n\n";
+
+my $req = GET $url;
+$req->header("User-Agent" => "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0");
+$req->header("Connection" => "close");
+$req->header("Accept-Language" => "en-GB,en;q=0.5");
+$req->header("Accept-Encoding" => "gzip, deflate");
+$req->header("Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+my $resp = $ua->request($req);
+
+print "Status code : ".$resp->code."\n";
+print "Response body : ".$resp->content."\n";
