0d2d6d30a4ba9c96b56bdd27e297d07c64022806
Robbie <txt3rob@gmail.com>
2016-07-07 09:31:40 +0100

    Update sms.rb

--- Examples/Ruby/sms.rb
+++ Examples/Ruby/sms.rb
@@ -1,22 +1,17 @@
-require 'net/http'
-require 'uri'
- 
-hash = "YOURHASH"
-messsage = "Your message"
-numbers = "447123456789"
+require "net/http"
+require "uri"
 
- 
-requested_url = 'http://www.smspi.co.uk/api/send/' +
-"&hash=" + hash + 
-"&to=" + numbers + "&message=" + URI.escape(message)
-             
- 
-url = URI.parse(requested_url)
-full_path = (url.query.blank?) ? url.path : "#{url.path}?#{url.query}"
-the_request = Net::HTTP::Get.new(full_path)
- 
-the_response = Net::HTTP.start(url.host, url.port) { |http|
-http.request(the_request)
-}
- 
-puts(the_response.body)
+uri = URI.parse("http://www.smspi.co.uk/api/send/")
+uri.query = URI.encode_www_form({"report"=>"no","to"=>"YOURNUMBER","message"=>"YOURMESSAGE","hash"=>"YOURHASH","flash"=>"no"})
+http = Net::HTTP.new(uri.host, uri.port)
+
+request = Net::HTTP::Get.new(uri.request_uri)
+request["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0"
+request["Connection"] = "close"
+request["Accept-Language"] = "en-GB,en;q=0.5"
+request["Accept-Encoding"] = "gzip, deflate"
+request["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+response = http.request(request)
+
+puts "Status code: "+response.code
+puts "Response body: "+response.body
