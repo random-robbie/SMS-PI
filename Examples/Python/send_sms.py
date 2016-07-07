6743904e25d2ea69139efa1551c9b7244bb0ea70
Robbie <txt3rob@gmail.com>
2016-07-07 09:30:37 +0100

    Update send_sms.py

--- Examples/Python/send_sms.py
+++ Examples/Python/send_sms.py
@@ -1,64 +1,16 @@
-#!/usr/bin/python
-#-----------------------------------
-# Send SMS Text Message
-#
-# Author : Robert Wiggins
-# Site   : http://www.smspi.co.uk
-# Date   : 22/07/2015
-#
-# Altered Script from Matt Hawkins of http://www.raspberrypi-spy.co.uk/
-# 
-#
-#-----------------------------------
+##
+#Send SMS By Robbie Wiggins#
+#Replace YOURNUMBER with who you want to send it to and REPLACE YOURMESSAGE with the message and REPLACE YOURHASH with your hash#
+##
 
-# Import required libraries
-import urllib      # URL functions
-import urllib2     # URL functions
-import sys		   # System Function
-import json		   # Json Parse
 
-# Validate the arguments have been set
-if(len(sys.argv) < 2):
-	exit('To use this please do the following...... python send_sms.py 07712312313 "this is the message required to be sent"') 
+import requests
 
-# Arguments Taken from command line
-to = sys.argv[1]
-message = sys.argv[2]
+session = requests.Session()
+print "Sending SMS"
+paramsGet = {"report":"no","to":"YOURNUMBER","message":"YOURMESSAGE","hash":"YOURHASH","flash":"no"}
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0","Connection":"close","Accept-Language":"en-GB,en;q=0.5","Accept-Encoding":"gzip, deflate","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
+response = session.get("https://www.smspi.co.uk/api/send/", params=paramsGet, headers=headers)
 
-### Set to Yes if you wish to send a flash (class 0) message
-FLASH = "NO"
-hash = "YOUR SMSPI API HASH"
-
-#-----------------------------------
-# No need to edit anything below this line
-#-----------------------------------
-
-values = {
-          'to' : to,
-          'message' : message,
-          'hash' : hash,
-          'flash' : FLASH} 
-          # Grab your hash from http://www.smspi.co.uk
-
-url = 'http://www.smspi.co.uk/api/send/'
-
-postdata = urllib.urlencode(values)
-req = urllib2.Request(url, postdata)
-
-print 'Attempt to send SMS ...'
-
-try:
-  response = urllib2.urlopen(req)
-  response_url = response.geturl()
-  if response_url==url:
-	data = json.load(response)
-	errorvalue = data[0]['error']
-	if errorvalue  == False:
-	 info = data[0]['info']
-	 print "SMS Status: " + info + "\n" + "To: " + to
-	else:
-	 smserror = data[0]['message']
-	 print "Error: " + smserror
-except urllib2.URLError, e:
-  print 'Send failed!'
-  print e.reason
+print "Status code:", response.status_code
+print "Response body:", response.content
